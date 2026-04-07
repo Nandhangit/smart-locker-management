70bd12556f11905fa13f3ef4027fc14fdc93f89a
Nandhangit <snandhan825@gmail.com>
2026-04-07 13:08:00 +0530

    Update API URL for production and add vercel config

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
-});
+  baseURL: 'https://smart-locker-backend-1o6f.onrender.com/api',});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('access_token');
